40497d95504cfc2ed8fcf68657d2407dced4b382
Imaad747 <imaadtheawesome@gmail.com>
2026-02-04 20:00:08 +1100

    Add files via upload

---
+++ addressbook.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from tkinter.ttk import *
+window=Tk()
+window.title("Address Book")
+
+def edit():
+
+    pass
+
+
+bookname=Label(window,text="My Address Book",width=35)
+
+bookname.grid(row=0,column=1,pady=10,columnspan=3)
+
+
+openbutton=Button(window,text="Open")
+
+openbutton.grid(row=0,column=3,pady=10)
+
+contacts_list=Listbox(window,width=30,height=15)
+contacts_list.grid(row=2,column=0,columnspan=3,rowspan=5)
+
+name_label=Label(window,text="Name:")
+name_label.grid(row=2,column=3)
+
+name=Entry(window)
+name.grid(row=2,column=4,padx=5)
+
+
+address_label=Label(window,text="Address:")
+address_label.grid(row=3,column=3)
+
+address=Entry(window)
+address.grid(row=3,column=4,padx=5)
+
+
+phone_number=Label(window,text="Phone Number:")
+phone_number.grid(row=4,column=3)
+
+numberphone=Entry(window)
+numberphone.grid(row=4,column=4,padx=5)
+
+email_label=Label(window,text="Email Address:")
+email_label.grid(row=5,column=3)
+
+email=Entry(window)
+email.grid(row=5,column=4,padx=5)
+
+birthday_label=Label(window,text="Birthday:")
+birthday_label.grid(row=6,column=3)
+
+birthday=Entry(window)
+birthday.grid(row=6,column=4,padx=5)
+
+edit_button=Button(window,text="Edit",width=10,command=edit)
+edit_button.grid(row=7,column=0,padx=12,pady=12)
+
+delete_button=Button(window,text="Delete",width=10)
+delete_button.grid(row=7,column=1,pady=12)
+
+update_button=Button(window,text="Update/Add",width=12)
+update_button.grid(row=7,column=4,pady=12)
+
+save_button=Button(window,text="Save",width=10)
+save_button.grid(row=8,column=1,pady=12)
+
+
+
+
+
+
+mainloop()
+
+
+
+
